a51f8bd3c6ab111aeb7ad4b4bd867e960ba0dcd7
Ajay Fewell <afewell2@illinois.edu>
2020-01-06 23:28:33 -0500

    Added files for the rest of the units for maintainability

---
+++ Landscaper.java
@@ -0,0 +1,10 @@
+package FirstPlayer;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Landscaper {
+    public static void run(RobotController rc) throws GameActionException {
+
+    }
+}
---
+++ NetGun.java
@@ -0,0 +1,10 @@
+package FirstPlayer;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class NetGun {
+    public static void run(RobotController rc) throws GameActionException {
+
+    }
+}
